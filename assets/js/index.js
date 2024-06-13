4ad3cca543dc0c9859272496208e45457470b248
JSchindleruibk <77682843+JSchindleruibk@users.noreply.github.com>
2024-06-13 12:17:21 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 window.onload = () => {
-    const header_rough = RoughNotation.annotate(document.querySelector("#oer-header > a > span"), { type: 'underline', color: 'cyan', strokeWidth: 3 });
+    const header_rough = RoughNotation.annotate(document.querySelector("#oer-header > a > span"), { type: 'underline', color: 'AliceBlue', strokeWidth: 3 });
     header_rough.show();
 
-    const main_rough = RoughNotation.annotate(document.querySelector("main > h1"), { type: 'highlight', color: 'cyan', multiline: true });
+    const main_rough = RoughNotation.annotate(document.querySelector("main > h1"), { type: 'highlight', color: 'AliceBlue', multiline: true });
     main_rough.show();
 
     try {
-        const event_info_rough = RoughNotation.annotate(document.querySelector("#oer-event-info > div"), { type: 'box', color: 'cyan', strokeWidth: 3 });
+        const event_info_rough = RoughNotation.annotate(document.querySelector("#oer-event-info > div"), { type: 'box', color: 'AliceBlue', strokeWidth: 3 });
         event_info_rough.show();
     } catch (error) {
         // ignored
@@ -14,7 +14,7 @@ window.onload = () => {
 
     const headings = document.querySelectorAll("h3");
     headings.forEach((heading) => {
-        const rn = RoughNotation.annotate(heading, { type: 'highlight', color: 'cyan', multiline: true });
+        const rn = RoughNotation.annotate(heading, { type: 'highlight', color: 'AliceBlue', multiline: true });
         rn.show();
     });
 };
